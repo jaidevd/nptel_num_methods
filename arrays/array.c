0e10f2213748df435a2ff97f5e960114a5081e5e
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2014-12-15 19:31:09 +0530

    Make a CLion project

---
+++ arrays/array.c
@@ -0,0 +1,19 @@
+/*
+ * array.c
+ * Copyright (C) 2014 jaidevd <deshpande.jaidev@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int num[] = {10, 20, 30, 40, 50};
+    int i, *j;
+    j = num;
+    for (i=0;i<5;i++){
+        printf("%lu %lu %d %d\n", &j, &j[i], *j, num[i]);
+        j++;
+    }
+}
